7c74e9738de3caee9c8baa2d08f0040acd3efe32
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-02-27 18:11:46 -0400

    [BE] Creates shared infrastructure folder

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { BootstrapServer } from './infrastructure/common/bootstrap-server.service'
+import { BootstrapServer } from './infrastructure/shared/bootstrap-server.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
