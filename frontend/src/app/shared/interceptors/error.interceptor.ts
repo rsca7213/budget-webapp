091c8885961c28ee36f53fb0e388ee64c7d59f0e
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-03-07 19:53:13 -0400

    [FE] Redirects to login on 401 response, fixes bug with form errors

--- frontend/src/app/shared/interceptors/error.interceptor.ts
+++ frontend/src/app/shared/interceptors/error.interceptor.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core'
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http'
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http'
 import { Observable, catchError, throwError } from 'rxjs'
 import { APP_PROPERTIES } from '../../../properties'
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(this.handleError))
   }
 
@@ -13,8 +19,17 @@ export class ErrorInterceptor implements HttpInterceptor {
     if (error.error instanceof ErrorEvent) {
       return throwError(() => new Error('Could not connect to the server, please try again later.'))
     } else {
+      if (!error.url?.includes('/login') && error.status === 401) {
+        window.location.href = '/login'
+      }
       if (APP_PROPERTIES.debug)
-        console.log('ErrorInterceptor', 'message:', error.error.message, 'details:', error.error.details)
+        console.log(
+          'ErrorInterceptor',
+          'message:',
+          error.error.message,
+          'details:',
+          error.error.details
+        )
       return throwError(() => new Error(error.error.message))
     }
   }
