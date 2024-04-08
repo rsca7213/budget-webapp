1bf614bcda59f8c85bb15d9c55088c0a09c8a504
Ricardo Salvatorelli <rsca4321@gmail.com>
2024-04-08 17:30:49 -0400

    [Web] Remove deprecated RouterTestingModule from tests

--- web/src/app/shared/guards/auth.guard.spec.ts
+++ web/src/app/shared/guards/auth.guard.spec.ts
@@ -2,7 +2,6 @@ import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/ro
 import { AuthService } from '../services/auth.service'
 import { authGuard } from './auth.guard'
 import { TestBed } from '@angular/core/testing'
-import { RouterTestingModule } from '@angular/router/testing'
 import { testData } from '../../../test/testing-data'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 
@@ -14,7 +13,7 @@ describe('Shared/AuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [HttpClientTestingModule],
       providers: [AuthService]
     })
 
